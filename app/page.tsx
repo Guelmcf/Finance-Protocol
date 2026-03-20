a511b31c6331d329ff1edbbf779747456807e33f
Miguel Chaves <guelmcf@gmail.com>
2026-03-20 09:22:53 -0300

    Feature: Add Prisma and configure the initial DB

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="">Hello World</div>
+    <div className="justify-center items-center flex h-screen">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+    </div>
   );
 }
